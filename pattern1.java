140bf07bafabcb05c475909bbe1180eabd2c1ec1
245123751033 <245123751033@mvsrec.edu.in>
2026-03-23 14:59:38 +0530

    Update pattern1.java

--- pattern1.java
+++ pattern1.java
@@ -1,6 +1,6 @@
 public class pattern1 {
     public static void main(String[] args) {
-        int n = 5; // fixed value
+        int n = 5;
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
